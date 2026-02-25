014352f807a4cc2669387184f98fee8fba6da70c
santiGalarza <s4n7ini7o@gmail.com>
2026-02-24 23:10:29 -0300

    Initial commit

---
+++ src/test/java/com/santiGalarza/order_management/OrderManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiGalarza.order_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
