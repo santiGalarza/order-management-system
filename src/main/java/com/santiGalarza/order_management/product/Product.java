601cbce5b8c43f4f97c7fee05f56adbebf8ce2c8
santiGalarza <s4n7ini7o@gmail.com>
2026-03-10 00:51:11 -0300

    feat: model product class

---
+++ src/main/java/com/santiGalarza/order_management/product/Product.java
@@ -0,0 +1,54 @@
+package com.santiGalarza.order_management.product;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.context.annotation.Primary;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @Column(precision = 8, scale = 2)
+    @NotNull
+    private BigDecimal price;
+
+    @Size(min = 8, max = 12)
+    @Column(unique = true)
+    @NotEmpty
+    @NotNull
+    private String sku;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private boolean isActive;
+    private int minOrderQuantity;
+    private int stockQuantity;
+    private float weight;
+
+    protected Product(){}
+
+}
