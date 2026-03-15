5968db1268f3a69a9e4f075ad21c00d5c07df915
santiGalarza <s4n7ini7o@gmail.com>
2026-03-14 21:50:59 -0300

    feat: add update mapping function

--- src/main/java/com/santiGalarza/order_management/product/ProductMapper.java
+++ src/main/java/com/santiGalarza/order_management/product/ProductMapper.java
@@ -1,9 +1,11 @@
 package com.santiGalarza.order_management.product;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper
 public interface ProductMapper {
-    ProductResponseDto productToProductResponseDTO(Product product);
+    ProductResponseDto toProductResponseDto(Product product);
     Product productRequestDTOtoProduct(ProductRequestDto productRequestDTO);
+    void updateProductRequestDto(ProductRequestDto productRequestDTO,@MappingTarget Product product);
 }
