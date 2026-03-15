eaaec08857fe8eb1cbb8145c3dfe42fb35bb7a51
santiGalarza <s4n7ini7o@gmail.com>
2026-03-15 01:58:32 -0300

    feat: add mapper method for patch requests

--- src/main/java/com/santiGalarza/order_management/product/ProductMapper.java
+++ src/main/java/com/santiGalarza/order_management/product/ProductMapper.java
@@ -1,11 +1,15 @@
 package com.santiGalarza.order_management.product;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper
 public interface ProductMapper {
     ProductResponseDto toProductResponseDto(Product product);
     Product productRequestDTOtoProduct(ProductRequestDto productRequestDTO);
     void updateProductRequestDto(ProductRequestDto productRequestDTO,@MappingTarget Product product);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void patchProductFromRequestDto(ProductPatchRequestDto productPatchRequestDto, @MappingTarget Product product);
 }
