9e71954cbe0d24badf8454eb53ab524b1abca1e0
santiGalarza <s4n7ini7o@gmail.com>
2026-03-14 21:49:53 -0300

    fix(repository): change class to interface

--- src/main/java/com/santiGalarza/order_management/product/ProductRepository.java
+++ src/main/java/com/santiGalarza/order_management/product/ProductRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public abstract class ProductRepository implements JpaRepository<Product, UUID> {
+public interface ProductRepository extends JpaRepository<Product, UUID> {
 }
