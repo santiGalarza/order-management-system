6ad781a8ac6b0a35bd8660aa6e5943b9382f042b
santiGalarza <s4n7ini7o@gmail.com>
2026-03-24 13:01:08 -0300

    fix: add @NoArgsConstructor to DTOs for deserialization

--- src/main/java/com/santiGalarza/order_management/order/OrderItemRequestDto.java
+++ src/main/java/com/santiGalarza/order_management/order/OrderItemRequestDto.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
@@ -12,10 +13,11 @@ import java.util.UUID;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class OrderItemRequestDto {
 
     private UUID productId;
 
     @Min(1)
-    private int quantity;
+    private Integer quantity;
 }
