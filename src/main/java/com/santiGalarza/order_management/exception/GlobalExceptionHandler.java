29263421eb6a62c10d0ded0bbd8d3505ca41963f
santiGalarza <s4n7ini7o@gmail.com>
2026-05-02 00:47:56 -0300

    feat(GlobalExceptionHandler): replace errorResponse dto with ProblemDetail class (RFC 9457)

--- src/main/java/com/santiGalarza/order_management/exception/GlobalExceptionHandler.java
+++ src/main/java/com/santiGalarza/order_management/exception/GlobalExceptionHandler.java
@@ -2,9 +2,10 @@ package com.santiGalarza.order_management.exception;
 
 import com.santiGalarza.order_management.product.InsufficientStockException;
 
-import jakarta.persistence.EntityNotFoundException;
-
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -12,56 +13,66 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
+import java.net.URI;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ErrorResponseDto> handleNotFoundException(EntityNotFoundException e) {
-        return build(HttpStatus.NOT_FOUND, e.getMessage());
-    }
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponseDto> handleValidationException(MethodArgumentNotValidException e) {
+    public ResponseEntity<ProblemDetail> handleValidationException(
+            MethodArgumentNotValidException e, HttpServletRequest req) {
         String errors = e.getBindingResult()
                 .getFieldErrors()
                 .stream()
                 .map(ex -> ex.getField() + ": " + ex.getDefaultMessage())
                 .collect(Collectors.joining(", "));
 
-        return build(HttpStatus.BAD_REQUEST, errors);
+        return build(HttpStatus.BAD_REQUEST, "Validation Failed", errors, req);
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<ErrorResponseDto> handleMethodNotSupported(HttpRequestMethodNotSupportedException e) {
-        return build(HttpStatus.METHOD_NOT_ALLOWED, e.getMessage());
+    public ResponseEntity<ProblemDetail> handleMethodNotSupported(
+            HttpRequestMethodNotSupportedException e, HttpServletRequest req) {
+        return build(HttpStatus.METHOD_NOT_ALLOWED, "Method Not Allowed", e.getMessage(), req);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ErrorResponseDto> handleMalformedJson(HttpMessageNotReadableException e) {
-        return build(HttpStatus.BAD_REQUEST, "Malformed JSON request");
+    public ResponseEntity<ProblemDetail> handleMalformedJson(
+            HttpMessageNotReadableException e, HttpServletRequest req) {
+        return build(HttpStatus.BAD_REQUEST, "Malformed Request", "Malformed JSON request",req);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponseDto> handleGeneric(Exception e) {
-        return build(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred");
+    public ResponseEntity<ProblemDetail> handleGeneric
+            (Exception e, HttpServletRequest req) {
+        return build(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error","An unexpected error occurred",req);
     }
 
     @ExceptionHandler(InsufficientStockException.class)
-    public ResponseEntity<ErrorResponseDto> handleInsufficientStock(InsufficientStockException e){
-        return build(HttpStatus.CONFLICT, e.getMessage());
+    public ResponseEntity<ProblemDetail> handleInsufficientStock(
+            InsufficientStockException e, HttpServletRequest req) {
+        return build(HttpStatus.CONFLICT, "Insufficient Stock", e.getMessage(), req);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponseDto> handleResourceNotFound(ResourceNotFoundException e){
-        return build(HttpStatus.NOT_FOUND, e.getMessage());
+    public ResponseEntity<ProblemDetail> handleResourceNotFound(
+            ResourceNotFoundException e, HttpServletRequest req){
+        return build(HttpStatus.NOT_FOUND, "Resource Not Found", e.getMessage(), req);
     }
 
-    private ResponseEntity<ErrorResponseDto> build(HttpStatus status, String message) {
-        return ResponseEntity.status(status).body(
-                new ErrorResponseDto(message, status.value(), LocalDateTime.now())
-        );
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ProblemDetail> handleDataIntegrity(
+            DataIntegrityViolationException e, HttpServletRequest request) {
+        return build(HttpStatus.CONFLICT, "Data Integrity Violation", "Data integrity violation", request);
+    }
+
+    private ResponseEntity<ProblemDetail> build(
+            HttpStatus status, String title, String detail, HttpServletRequest req) {
+        ProblemDetail problem = ProblemDetail.forStatusAndDetail(status, detail);
+        problem.setTitle(title);
+        problem.setInstance(URI.create(req.getRequestURI()));
+
+        return ResponseEntity.status(status).body(problem);
     }
 }
