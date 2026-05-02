c1be639ac27aec2ec6fefc6802259322dcce808b
santiGalarza <s4n7ini7o@gmail.com>
2026-05-02 00:54:32 -0300

    fix: changed insufficient stock exception to only mention product id

--- src/main/java/com/santiGalarza/order_management/product/InsufficientStockException.java
+++ src/main/java/com/santiGalarza/order_management/product/InsufficientStockException.java
@@ -3,7 +3,7 @@ package com.santiGalarza.order_management.product;
 import java.util.UUID;
 
 public class InsufficientStockException extends RuntimeException {
-    public InsufficientStockException(UUID id, int quantity) {
-        super("Product with id "+id+"has a total stock of "+quantity);
+    public InsufficientStockException(UUID id) {
+        super("Product with id "+id+" has insufficient stock");
     }
 }
