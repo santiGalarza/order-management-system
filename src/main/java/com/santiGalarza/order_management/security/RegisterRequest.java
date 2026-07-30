837799add42e6d74d2ab1d60cb53ee8bd273631c
santiGalarza <s4n7ini7o@gmail.com>
2026-07-30 18:17:41 -0300

    feat(security): login & request dtos

--- src/main/java/com/santiGalarza/order_management/security/RegisterRequest.java
+++ src/main/java/com/santiGalarza/order_management/security/RegisterRequest.java
@@ -2,6 +2,7 @@ package com.santiGalarza.order_management.security;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,12 +10,18 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class LoginRequest {
-
+public class RegisterRequest {
     @Email
     @NotBlank
     private String email;
 
     @NotBlank
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
 }
