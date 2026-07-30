f68cd15399ad3efa0149afca4351f6de612f10ca
santiGalarza <s4n7ini7o@gmail.com>
2026-07-30 18:03:48 -0300

    feat(auth): security config

---
+++ src/main/java/com/santiGalarza/order_management/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.santiGalarza.order_management.security;
+
+import com.santiGalarza.order_management.user.UserRepository;
+import lombok.NonNull;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(@NonNull String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email "+email));
+    }
+}
