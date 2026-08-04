f6dc0974371576f6812c6da3a83baa43a6f36f70
santiGalarza <s4n7ini7o@gmail.com>
2026-08-04 03:28:45 -0300

    fix(orders): item & order mappers

--- src/main/java/com/santiGalarza/order_management/order/item/ItemMapper.java
+++ src/main/java/com/santiGalarza/order_management/order/item/ItemMapper.java
@@ -1,9 +1,15 @@
 package com.santiGalarza.order_management.order.item;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ItemMapper {
+
+    @Mapping(source = "product.id", target = "productId")
+    @Mapping(source = "product.name", target = "productName")
+    @Mapping(expression = "java(item.getUnitPrice().multiply(java.math.BigDecimal.valueOf(item.getQuantity())))", target = "totalPrice")
     ItemResponse toResponseDto(Item item);
+
     Item toEntity(CreateItemRequest createItemRequest);
 }
