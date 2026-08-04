05bfdd593f7ec787052b56bd5b0d9aee5d41a8b9
santiGalarza <s4n7ini7o@gmail.com>
2026-08-04 03:27:33 -0300

    fix(auth): explicityl handle exception in security filter chain with UNAUTHORIZED

--- src/main/java/com/santiGalarza/order_management/security/SecurityConfig.java
+++ src/main/java/com/santiGalarza/order_management/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.santiGalarza.order_management.security;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -35,6 +36,9 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint((request, response, authException) ->
+                                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated())
