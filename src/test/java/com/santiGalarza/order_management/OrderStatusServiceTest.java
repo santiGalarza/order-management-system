21714d58c160a34157e79713616c406bc03a7889
santiGalarza <s4n7ini7o@gmail.com>
2026-08-30 00:50:17 -0300

    test: initial order transition test

---
+++ src/test/java/com/santiGalarza/order_management/OrderStatusServiceTest.java
@@ -0,0 +1,41 @@
+package com.santiGalarza.order_management;
+
+import com.santiGalarza.order_management.order.status.OrderStatusHistoryRepository;
+import com.santiGalarza.order_management.order.status.OrderStatusRepository;
+import com.santiGalarza.order_management.order.status.OrderStatusService;
+import com.santiGalarza.order_management.order.status.OrderStatusTransitionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderStatusServiceTest {
+
+    @Mock
+    private OrderStatusRepository statusRepository;
+
+    @Mock
+    private OrderStatusTransitionRepository transitionRepository;
+
+    @Mock
+    private OrderStatusHistoryRepository historyRepository;
+
+    @InjectMocks
+    private OrderStatusService orderStatusService;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    @DisplayName("throws when status code not found")
+    void throwsWhenStatusCodeNotFound() {
+
+    }
+
+}
