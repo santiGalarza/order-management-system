01e5d5e3fb5c3de2372550d8c1e7f51010c73a50
santiGalarza <s4n7ini7o@gmail.com>
2026-09-03 16:24:23 -0300

    refactor(itemRepository): remove unused repository methods

--- src/main/java/com/santiGalarza/order_management/order/item/ItemRepository.java
+++ src/main/java/com/santiGalarza/order_management/order/item/ItemRepository.java
@@ -7,7 +7,5 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface ItemRepository extends JpaRepository<Item, UUID> {
-    List<Item> findByOrderId(UUID orderId);
-    List<Item> findByIdInAndOrderId(List<UUID> itemIds, UUID orderId);
     Optional<Item> findByIdAndOrderId(UUID itemId, UUID orderId);
 }
